50fdf1d8ec3e64ad24b5ce35e34cbe46117d03d3
miralyaser9 <https://github.com/miralyaser9>
2024-04-17 01:29:01 +0200

    solving the issue of main.dart related to language changing

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:graduation_project/features/profile/presentation/manager/change_language_cubit.dart';
 import 'core/routes/app_routes.dart';
 import 'generated/l10n.dart';
 
@@ -11,30 +12,44 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyAppWithLanguage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ChangeLanguageCubit, ChangeLanguageState>(
+      builder: (context, state) {
+        return MaterialApp(
+          theme: ThemeData(
+              textTheme: const TextTheme()),
+
+          locale:  Locale(BlocProvider.of<ChangeLanguageCubit>(context).languageCode),
+          localizationsDelegates: const [
+            S.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: S.delegate.supportedLocales,
+          debugShowCheckedModeBanner: false,
+          initialRoute: Routes.splash,
+          onGenerateRoute: AppRoutes.onGenerateRoutes,
+        );
+      },
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
 
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: Size(360, 690),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      child: MaterialApp(
-        theme: ThemeData(
-            textTheme: const TextTheme()),
-
-        locale:  Locale('en'),
-        localizationsDelegates: const [
-          S.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: S.delegate.supportedLocales,
-        debugShowCheckedModeBanner: false,
-        initialRoute: Routes.splash,
-        onGenerateRoute: AppRoutes.onGenerateRoutes,
+    return BlocProvider(
+      create: (context) => ChangeLanguageCubit(),
+      child: ScreenUtilInit(
+        designSize: Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        child: MyAppWithLanguage(),
       ),
     );
   }
