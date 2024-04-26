8177bb6ed7b1cba2f25cd52bdcc97efbc0c2185b
HW <khaleddortmound123@gmail.com>
2024-04-26 05:00:44 +0300

    i solved the problem of add post screen

--- lib/features/community/presentation/screens/add_post_screen.dart
+++ lib/features/community/presentation/screens/add_post_screen.dart
@@ -7,6 +7,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:graduation_project/core/commons/global_cubits/global_community_bloc/global_community_bloc_cubit.dart';
+import 'package:intl/intl.dart';
 
 import '../../../../core/utilis/app_styles.dart';
 import '../../../../core/utilis/colors.dart';
@@ -14,7 +15,9 @@ import '../../../../core/utilis/image_constants.dart';
 import '../widgets/camera_post_container.dart';
 
 class AddPostScreen extends StatelessWidget {
-  const AddPostScreen({super.key});
+  AddPostScreen({super.key});
+
+
 
 
 
@@ -28,104 +31,83 @@ class AddPostScreen extends StatelessWidget {
   builder: (context, state) {
     final communityBloc=BlocProvider.of<GlobalCommunityBloc>(context);
     return Scaffold(
-      body:SingleChildScrollView(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height:MediaQuery.of(context).size.height,
-          child: Column(
-            children: [
-              SizedBox(height: 50.h,),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height:40.h,
-                child: Row(
-                  children: [
-                    Padding(
-                      padding:  EdgeInsetsDirectional.only(start: 20.w),
-                      child: TextButton(onPressed: (){}, child:
-                      GestureDetector(
-                        onTap: ()
-                        {
-                          Navigator.pop(context);
-                        },
-                        child: Text('Cancel',
-                          style: AppKhaledStyles.textStyle(
-                            color: AppColors.c4C9EEB,
-                            size: 16 ,
-                          ),),
-                      ),
-                      ),
-                    ),
-                    Spacer(),
-                    Container(
-                      width: 67.w,
-                      height: 34.h,
-                      decoration: BoxDecoration(
-                        color: AppColors.cB9DCF7,
-                        borderRadius: BorderRadius.circular(16.r),
-                      ),
-                      child: Center(
-                        child: Text('Post',style: AppKhaledStyles.textStyle(
-                          color: AppColors.white,
-                          size: 12,
-                        ),),
-                      ),
-                    ),
-                    SizedBox(width: 21.w,),
-                  ],
-                ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 363.h,
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle
-                ),
-                child: Padding(
+      body:Column(
+        children: [
+          SizedBox(height: 50.h,),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height:40.h,
+            child: Row(
+              children: [
+                Padding(
                   padding:  EdgeInsetsDirectional.only(start: 20.w),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      prefixIcon: Image.asset(ImageConstants.roundPostTwitter),
-                      hintText: 'What’s happening?',
-                      hintStyle: AppKhaledStyles.textStyle(
-                        color: AppColors.c687684,
-                        size: 15,
-                      ),
-
-
-                    ),
-
+                  child: TextButton(onPressed: (){}, child:
+                  GestureDetector(
+                    onTap: ()
+                    {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Cancel',
+                      style: AppKhaledStyles.textStyle(
+                        color: AppColors.c4C9EEB,
+                        size: 16 ,
+                      ),),
+                  ),
+                  ),
+                ),
+                Spacer(),
+                Container(
+                  width: 67.w,
+                  height: 34.h,
+                  decoration: BoxDecoration(
+                    color: AppColors.cB9DCF7,
+                    borderRadius: BorderRadius.circular(16.r),
+                  ),
+                  child: Center(
+                    child: Text('Post',style: AppKhaledStyles.textStyle(
+                      color: AppColors.white,
+                      size: 12,
+                    ),),
+                  ),
+                ),
+                SizedBox(width: 21.w,),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding:  EdgeInsetsDirectional.only(start: 20.w),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  prefixIcon: Image.asset(ImageConstants.roundPostTwitter),
+                  hintText: 'What’s happening?',
+                  hintStyle: AppKhaledStyles.textStyle(
+                    color: AppColors.c687684,
+                    size: 15,
                   ),
                 ),
               ),
-              Spacer(),
-              Row(
-                children:
-                [
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 94.h,
-                    child: ListView.separated(
-                      padding: EdgeInsetsDirectional.all(8.w),
-                      scrollDirection: Axis.horizontal,
-                        itemBuilder: (context, index) => CmaeraPostContainer(
-                          cameraPostsModedl: communityBloc.cameraPostsData[index],
-
-                        ),
-                        separatorBuilder: (context, index) => SizedBox(width: 12.w),
-                        itemCount: 4),
-                  )
-
-
-                ],
-              ),
-
-
-            ],
+            ),
           ),
-        ),
+          Spacer(),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 94.h,
+            child: ListView.separated(
+              padding: EdgeInsetsDirectional.all(8.w),
+              scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) => CmaeraPostContainer(
+                  cameraPostsModedl: communityBloc.cameraPostsData[index],
+
+                ),
+                separatorBuilder: (context, index) => SizedBox(width: 12.w),
+                itemCount: 4),
+          ),
+
+
+        ],
       )
     );
   },
