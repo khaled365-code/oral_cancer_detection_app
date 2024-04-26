cdbd53ba8a6af9e3acaeac8ab39fc96adbff57fb
HW <khaleddortmound123@gmail.com>
2024-04-26 05:06:56 +0300

    new commit

--- lib/features/community/presentation/screens/add_post_screen.dart
+++ lib/features/community/presentation/screens/add_post_screen.dart
@@ -7,7 +7,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:graduation_project/core/commons/global_cubits/global_community_bloc/global_community_bloc_cubit.dart';
-import 'package:intl/intl.dart';
 
 import '../../../../core/utilis/app_styles.dart';
 import '../../../../core/utilis/colors.dart';
