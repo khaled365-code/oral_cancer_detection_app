b70af4959f9f048d204b3ee86f0b1e43f4d72791
miralyaser9 <https://github.com/miralyaser9>
2024-05-18 22:21:03 +0300

    publock

--- lib/features/community/presentation/screens/community_screen.dart
+++ lib/features/community/presentation/screens/community_screen.dart
@@ -1,4 +1,3 @@
-import 'package:custom_refresh_indicator/custom_refresh_indicator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
