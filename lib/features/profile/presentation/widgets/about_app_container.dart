a198282969130538e87d10ce1c9d092c4f1ff0f4
miralyaser9 <https://github.com/miralyaser9>
2024-06-17 01:12:43 +0300

    mm

--- lib/features/profile/presentation/widgets/about_app_container.dart
+++ lib/features/profile/presentation/widgets/about_app_container.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/utilis/app_colors.dart';
-import '../../../../core/utilis/app_colors.dart';
 import '../../../../core/widgets/resuable_text.dart';
 
 class AboutAppContainerItem extends StatelessWidget {
