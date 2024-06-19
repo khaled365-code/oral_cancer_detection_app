8e83586a37d4e74bd7bbbd22b5a59d8183439a29
miralyaser9 <https://github.com/miralyaser9>
2024-06-20 02:33:05 +0300

    bottom nav bar finished

--- lib/features/home/presentation/views/home_screen.dart
+++ lib/features/home/presentation/views/home_screen.dart
@@ -178,25 +178,5 @@ class HomePageState extends State<HomePage> {
     }
 
   }
-
 }
 
-
-// bottomNavigationBar: Padding(
-// padding: const EdgeInsets.symmetric(vertical: 15),
-// child: CurvedNavigationBar(
-// height: 30.h,
-// onTap: onItemTapped,
-// animationDuration: Duration(milliseconds: 500),
-// backgroundColor: AppColors.white,
-// color: AppColors.white,
-// buttonBackgroundColor: AppColors.primary,
-// items: [
-// BottomNavColumn(paddingValue: 8,icon: Icons.home_outlined, text: 'Home'),
-// BottomNavColumn(paddingValue: 2,icon: FontAwesomeIcons.stethoscope, text: 'Diagnosis'),
-// BottomNavColumn(paddingValue: 3,icon: Icons.mark_unread_chat_alt, text: 'Community'),
-// BottomNavColumn(paddingValue: 5,icon: Icons.newspaper_sharp, text: 'News'),
-// BottomNavColumn(paddingValue: 5,icon:FontAwesomeIcons.hospital, text: 'Hospitals')
-// ]
-// ),
-// ),
